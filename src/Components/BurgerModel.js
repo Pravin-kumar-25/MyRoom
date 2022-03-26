edd9012b4bd514a106f86541a89f25aceacd321f
Pravin-kumar-25 <pravinkumar3725@gmail.com>
2022-03-26 16:10:28 +0530

    Add Orbit controls

--- src/Components/BurgerModel.js
+++ src/Components/BurgerModel.js
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, softShadows } from "@react-three/drei";
+import { OrbitControls, softShadows, PresentationControls } from "@react-three/drei";
 import { Suspense } from "react";
 import Models from "./Models";
 
@@ -14,6 +14,7 @@ const BurgerModel = () => {
             style={{ height: "100vh", width: "100vw" }}
         // ref={ref}
         >
+            {/* <PresentationControls global zoom={0.8} rotation={[0, -Math.PI / 4, 0]} polar={[0, Math.PI / 4]} azimuth={[-Math.PI / 4, Math.PI / 4]}> */}
             <ambientLight intensity={0.3} />
             <pointLight color={'yellow'} intensity={0.3} castShadow position={[-70,25,20]} />
             <pointLight color={'white'} castShadow
@@ -25,11 +26,13 @@ const BurgerModel = () => {
         // shadow-mapSize-height={512}
         // shadow-mapSize-width={512}
       /> */}
+
             <Suspense fallback={null}>
 
                 <Models />
             </Suspense>
-            <OrbitControls />
+      {/* </PresentationControls> */}
+            <OrbitControls autoRotateSpeed={1} enableDamping={true} enableZoom={false} minAzimuthAngle={Math.PI/2} maxAzimuthAngle={Math.PI} minPolarAngle={0} maxPolarAngle={Math.PI/2}/>
         </Canvas>
     )
 }
